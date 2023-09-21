102f9ebb0307831cfe961bae9c06d8dfb5a870a0
Jamil Orlando Gutierrez Artiga <jamil.gutierrez@medicamentos.gob.sv>
2023-09-21 16:13:50 -0600

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@include('layout.header')
+@include('layout.menu')
+
+{{-- contenido --}}
+
+<div class="container-lg container-sm">
+
+</div>
+
+@include('layout.footer')
---
+++ resources/views/layout/menu.blade.php
@@ -0,0 +1,31 @@
+<div class="col-12 d-flex flex-row justify-content-start justify-content-xl-center justify-content-lg-center justify-content-md-center">
+    <nav class="d-none d-md-flex d-lg-flex menu col-9 col-md-11 col-lg-8 col-md-10 mt-lg-4 mt-md-4 d-flex flex-row justify-content-between align-items-lg-center">
+        <span class="d-block menu-logo col-md-3 col-3 col-lg-2">CarLoan</span>
+        <ul class="col-lg-9 col-md-9 menu-items-container d-flex flex-row justify-content-between align-items-center">
+            <li><a class="menu-items-container-links" href="#">Equipos</a></li>
+            <li><a class="menu-items-container-links" href="#">Seguros</a></li>
+            <li><a class="menu-items-container-links" href="#">Beneficios</a></li>
+            <li><a class="menu-items-container-links" href="#">Ayuda</a></li>
+            <li><a class="menu-items-container-links" href="#">Preguntas</a></li>
+            <li>
+                <button type="button" class="btn btn-primary btn-lg">Cotiza</button>
+            </li>
+        </ul>
+
+    </nav>
+
+    <nav class="d-block col-12 d-md-none d-lg-none d-xl-none movil-menu  mt-4">
+        <span class="d-block menu-logo col-2">CarLoan</span>
+        <ul class="col-lg-9 col-md-9 menu-items-container d-flex flex-column justify-content-start align-items-start">
+            <li><a class="menu-items-container-links movil-menu-items" href="#">Equipos</a></li>
+            <li><a class="menu-items-container-links movil-menu-items" href="#">Seguros</a></li>
+            <li><a class="menu-items-container-links movil-menu-items" href="#">Beneficios</a></li>
+            <li><a class="menu-items-container-links movil-menu-items" href="#">Ayuda</a></li>
+            <li><a class="menu-items-container-links movil-menu-items" href="#">Preguntas</a></li>
+            <li>
+                <button type="button" class="btn btn-primary btn-lg">Cotiza</button>
+            </li>
+        </ul>
+
+    </nav>
+</div>
